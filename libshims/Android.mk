898efbc5b39e943d0f24c51289d145e1a2105ac7
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-05 08:35:30 -0600

    addison: add shim for wifi_qsap_set_tx_power
    Change-Id: I9ef277db6f4e068e7b92710f6039b4f261d72282

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -33,3 +33,13 @@ LOCAL_MODULE := libqsap_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+# wifi_qsap
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libqsap_shim.c
+LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog
+LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
+LOCAL_MODULE := libqsap_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
